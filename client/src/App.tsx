2dbb21d0962636c07b60ee1148b36d9fa1a2a963
harshchill <harshmahto02@gmail.com>
2025-04-27 21:42:32 +0530

    fix: remove duplicate QueryClientProvider to fix React Query initialization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import { CareerConfidenceProvider } from "./contexts/CareerConfidenceContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 function Router() {
@@ -18,15 +16,13 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <LanguageProvider>
-          <CareerConfidenceProvider>
-            <Router />
-          </CareerConfidenceProvider>
-        </LanguageProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <LanguageProvider>
+        <CareerConfidenceProvider>
+          <Router />
+        </CareerConfidenceProvider>
+      </LanguageProvider>
+    </TooltipProvider>
   );
 }
 
